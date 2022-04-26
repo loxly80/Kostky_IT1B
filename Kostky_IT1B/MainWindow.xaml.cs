aace6308bf164b416d62d764634fc8757df26ad5
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-04-26 10:07:45 +0200

    před hodinou

--- Kostky_IT1B/MainWindow.xaml.cs
+++ Kostky_IT1B/MainWindow.xaml.cs
@@ -40,6 +40,45 @@ namespace Kostky_IT1B
         public MainWindow()
         {
             InitializeComponent();
+            ZobrazKostky();
+        }
+
+        private void ZobrazKostky()
+        {
+            kostka1.Content = kostky[0].Hodnota;
+            kostka2.Content = kostky[1].Hodnota;
+            kostka3.Content = kostky[2].Hodnota;
+            kostka4.Content = kostky[3].Hodnota;
+            kostka5.Content = kostky[4].Hodnota;
+            kostka6.Content = kostky[5].Hodnota;
+        }
+
+        private void btnHod_Click(object sender, RoutedEventArgs e)
+        {
+            foreach(var kostka in kostky)
+            {
+                kostka.Hod();
+            }
+            ZobrazKostky();
+            SpocitejBody();
+        }
+
+        private int SpocitejBody()
+        {
+            int body = 0;
+            Dictionary<int, int> pocty = new Dictionary<int, int>();
+            pocty.Add(1, 0);
+            pocty.Add(2, 0);
+            pocty.Add(3, 0);
+            pocty.Add(4, 0);
+            pocty.Add(5, 0);
+            pocty.Add(6, 0);
+
+            foreach (var kostka in kostky)
+            {
+                pocty[kostka.Hodnota]++;
+            }
+            return body;
         }
     }
 }
